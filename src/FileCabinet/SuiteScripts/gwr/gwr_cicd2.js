8d89d7a16d1c3277e32588614c60b049e8e3ae43
Geoffrey Routledge <geoff@dgbi.ca>
2023-09-13 11:57:03 -0700

    Update gwr_cicd2.js

--- src/FileCabinet/SuiteScripts/gwr/gwr_cicd2.js
+++ src/FileCabinet/SuiteScripts/gwr/gwr_cicd2.js
@@ -4,8 +4,9 @@
  */
 define(['N/record', 'N/search'],
 
-// 20230913-114355DA072285-C96F-47BE-8783-0A1B3BB7326D
-// 20230913-104350
+
+// __________________
+// 20230913-115649
        
  * @param{record} record
  * @param{search} search
