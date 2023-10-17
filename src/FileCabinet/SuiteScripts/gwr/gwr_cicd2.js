a878ccb49a90e2d8990ddf54a7a4bda480cc6669
Geoffrey Routledge <geoff@dgbi.ca>
2023-10-17 11:27:10 -0700

    20231017-112708

--- src/FileCabinet/SuiteScripts/gwr/gwr_cicd2.js
+++ src/FileCabinet/SuiteScripts/gwr/gwr_cicd2.js
@@ -4,16 +4,6 @@
  */
 define(['N/record', 'N/search'],
 
-// 20230915-191826
-  //     20230919-145638
-       // 20230925-144024
-       // 20230925-144315
-       // 20230925-144800
-       // 20230925-145202 
-       // 20230925-223906
-       // 20230925-223957
-       // geoff's changes
-       // my changes are better than your changes!!!
        // 20231004-152916
 /*     
  * @param{record} record
@@ -29,12 +19,9 @@ define(['N/record', 'N/search'],
          * @since 2015.2
          */
 
-        const onRequest = (scriptContext) => { // more code from geoff
-//kgr code
-//more changes for me.
+        const onRequest = (scriptContext) => { 
         }
 
         return {onRequest}
 
     });
-// 20230913-010311 178E4999-885F-4E88-BD59-0C98EBECB0C3
